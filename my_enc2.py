7c52dc82cf9c3d34d2e756414e605d0b3651de86
ok Noronha <ok.noronha.kevin@gmail.com>
2022-07-08 08:55:28 +0530

    v1.0.6

---
+++ my_enc2.py
@@ -0,0 +1,89 @@
+import random
+from PIL import Image, ImageDraw
+import os, sys, cv2
+import numpy as np
+
+share_files = []
+shares=[]
+temp_shares=[]
+
+
+
+def __init__():
+    if len(sys.argv)!=1:
+        print("This takes one argument; the image to be split.")
+        exit()
+    #infile=str(sys.argv[1])
+    global infile
+    infile ="dog.jpeg"
+
+    if not os.path.isfile(infile):
+        print("That file does not exist.")
+        exit()
+
+    global img
+    img = cv2.imread(infile)
+
+    global file
+    global ext
+    file, ext = os.path.splitext(infile)
+
+
+    global n; global k
+    #n=int(input("Number of Shares to be made :"))
+    #k=int(input("Number of Shares need to get picture :"))
+    n=5
+    k=3
+    global rows, columns, channels
+    rows, columns, channels = img.shape
+
+
+
+def create_shares():
+    for i in range(1,n+1):
+        share_files.append(file+str(i)+".png")
+        r=[]
+        for row in img:
+            col=[]
+            r.append(col)
+            for pix in row:
+                ch=[]
+                col.append(ch)
+                for channel in pix:
+                    ch.append(0)
+        shares.append(np.array(r))
+
+def share_mod():
+    global shares
+    global temp_shares
+    for row in range(rows):
+        for pix in range(columns):
+            for channel in range(channels):
+                val=img[row,pix,channel]
+                nums=[]
+                temp_shares=[]
+                for _ in range(0,k):
+                    num = random.randint(0,val)
+                    nums.append(num)
+                    share=shares.pop(random.randint(0,len(shares)-1))
+                    share[row,pix,channel]=num
+                    val-=num
+                    temp_shares.append(share)
+
+                for _ in range(0,n-k):
+                    share=shares.pop(random.randint(0,len(shares)-1))
+                    share[row,pix,channel]=max(nums)
+                    temp_shares.append(share)
+                shares=temp_shares
+
+
+
+def save_shares():
+    for i in range(len(shares)):
+        cv2.imwrite(share_files[i],shares[i])
+
+
+__init__()
+create_shares()
+share_mod()
+save_shares()
